a0e3f75cb8253685cce1cf65f01205b8d1256bae
Khushal Suthar <71877944+AreYouKhush@users.noreply.github.com>
2021-10-31 22:37:44 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,17 @@
 def binary_search(arr, low, high, x): 
   
     # Check base case 
-    if high >= low: 
+    if (high >= low): 
   
         mid = (high + low) // 2
   
         # If element is present at the middle itself 
-        if arr[mid] == x: 
+        if (arr[mid] == x): 
             return mid 
   
         # If element is smaller than mid, then it can only 
         # be present in left subarray 
-        elif arr[mid] > x: 
+        elif (arr[mid] > x): 
             return binary_search(arr, low, mid - 1, x) 
   
         # Else the element can only be present in right subarray 
